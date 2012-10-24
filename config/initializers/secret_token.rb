cee3cda73290fc9bb7c34e5b2bf2b01fdc75ca18
CryWolf <cry.wolf@centrum.cz>
2012-10-24 15:12:20 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ninetab::Application.config.secret_token = 'ab086efaee7c356db3ad26d5e0f793eccca99176790a9032dd54fe23ce3dcd2160fcd2f9bf58f9b8aaac74631d81a512c7343b2dde8f8be88d7a53cfcac59bc4'
